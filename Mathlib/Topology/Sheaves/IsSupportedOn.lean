38a5805c1d10dbad55a206a8f291e7c490b7e436
Brian-Nugent <bnugent@uw.edu>
2026-02-19 18:27:00 -0700

    feat(Topology/Sheaves): add initial implementation for sheaves supported on closed subsets

---
+++ Mathlib/Topology/Sheaves/IsSupportedOn.lean
@@ -0,0 +1,31 @@
+/-
+Copyright (c) 2021 Brian Nugent. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Brian Nugent
+-/
+module
+
+public import Mathlib
+
+/-!
+# Sheaves supported on closed subsets
+
+## Main definitions
+
+-/
+
+@[expose] public section
+
+noncomputable section
+
+universe u v w
+
+open CategoryTheory Limits TopologicalSpace
+
+namespace TopCat
+
+namespace Sheaf
+
+end Sheaf
+
+end TopCat
