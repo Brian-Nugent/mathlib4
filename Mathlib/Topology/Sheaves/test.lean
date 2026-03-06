cbb980cfde77d9a8f1be8316ea7a46c27ace31af
Brian-Nugent <bnugent@uw.edu>
2026-03-06 16:46:05 -0700

    feat: Add toRestrict

--- Mathlib/Topology/Sheaves/test.lean
+++ Mathlib/Topology/Sheaves/test.lean
@@ -279,49 +279,56 @@ end Sheaf
 
 end CategoryTheory
 
-namespace AlgebraicGeometry.Scheme.Modules
+open TopologicalSpace CategoryTheory Topology Opposite
 
-open TopCat Sheaf CategoryTheory AddCommGrpCat Limits
+@[simps!]
+def CategoryTheory.Adjunction.sheafPushforwardContinuous {C : Type*} [Category* C] {D : Type*}
+    [Category* D] {F : C ⥤ D} {G : D ⥤ C} (adj : F ⊣ G)
+    (J : GrothendieckTopology C) (K : GrothendieckTopology D) [F.IsContinuous J K]
+    [G.IsContinuous K J] (E : Type*) [Category* E] :
+    F.sheafPushforwardContinuous E J K ⊣ G.sheafPushforwardContinuous E K J where
+  unit.app P := { val := (adj.op.whiskerLeft _).unit.app P.val }
+  counit.app P := { val := (adj.op.whiskerLeft _).counit.app P.val }
+  left_triangle_components P := by
+    ext : 1
+    exact (adj.op.whiskerLeft _).left_triangle_components P.val
+  right_triangle_components P := by
+    ext : 1
+    exact (adj.op.whiskerLeft _).right_triangle_components P.val
 
-variable (n : ℕ) {X : Scheme.{u}} [IsAffine X] (F : X.Modules)
+variable (C : Type*) [Category* C] {X : TopCat.{u}} {Y : TopCat.{u}} {f : Y ⟶ X}
+  (hf : IsOpenEmbedding f)
 
-noncomputable abbrev sheaf : Sheaf Ab X :=
-  (SheafOfModules.toSheaf X.ringCatSheaf).obj F
+namespace TopCat.Sheaf
 
-/- noncomputable abbrev restrict (U : Opens X) :
-    TopCat.Sheaf AddCommGrpCat.{u} X ⥤ TopCat.Sheaf AddCommGrpCat.{u} X :=
-    Sheaf.pullback AddCommGrpCat.{u} (Opens.inclusion' U) ⋙ Sheaf.pushforward AddCommGrpCat
-    (Opens.inclusion' U) -/
+abbrev restrict : Sheaf C X ⥤ Sheaf C Y := by
+  haveI := hf.functor_isContinuous
+  exact hf.functor.sheafPushforwardContinuous C ..
 
-#check restrict
+abbrev restrictPushforwardAdjunction : restrict C hf ⊣ pushforward C f := by
+  haveI := hf.functor_isContinuous
+  exact Adjunction.sheafPushforwardContinuous hf.isOpenMap.adjunction ..
 
-noncomputable abbrev openRestrictPush (U : Opens X) : X.Modules ⥤ X.Modules :=
-  restrictFunctor U.ι ⋙ pushforward U.ι
+variable (F : Sheaf C X) (U V : Opens X)
 
-noncomputable abbrev toOpenRestrictPush (U : X.Opens) := (restrictAdjunction U.ι).unit
+abbrev toRestrict := (restrictPushforwardAdjunction C U.isOpenEmbedding).unit
 
-private lemma vanish_base : Subsingleton (H F.sheaf 1) := sorry
+example : ((restrict C U.isOpenEmbedding ⋙ pushforward C U.inclusion').obj F).val.obj (op V) =
+    F.val.obj (op (U.isOpenEmbedding.functor.obj ((Opens.map U.inclusion').obj V))) := rfl
 
+example : U.isOpenEmbedding.functor.obj ((Opens.map U.inclusion').obj V) = U ⊓ V := by aesop
 
-instance : Subsingleton (H F.sheaf (n + 1)) := by
-  revert F X
-  refine Nat.case_strong_induction_on (p := fun n => ∀ {X : Scheme} [IsAffine X] (F : X.Modules),
-    Subsingleton ((F.sheaf).H (n + 1))) n vanish_base ?_
-  intro n hi X _ F
-  apply subsingleton_of_forall_eq 0
-  intro s
-  obtain ⟨I, ⟨U, ⟨_, hU⟩⟩⟩ := Sheaf.prop1 F.sheaf (n + 1) (isBasis_affineOpens X) sorry (by
-    intro r U hr hrn hU
-    have : r - 1 < n := sorry
-    sorry) s
-  have : Finite I := sorry
-  let j : I → X.Modules := fun i => (openRestrictPush (U i)).obj F
-  let G : X.Modules := ∏ᶜ j
-  let res : F ⟶ G := Pi.lift (fun b => (toOpenRestrictPush (U b)).app F)
-  haveI : Mono res := sorry
-  let S := ShortComplex.mk res (cokernel.π res) (by cat_disch)
-  have := hi n (Nat.le_refl n) S.X₃
-  
-  sorry
+lemma toRestrict_app_val_all : ((toRestrict C U).app F).val.app (op V) =
+    F.val.map (U.isOpenEmbedding.isOpenMap.adjunction.counit.app V).op := by simp
 
-end AlgebraicGeometry.Scheme.Modules
+lemma toRestrict_app_val_app_epi [IsFlasque F] : Epi (((toRestrict C U).app F).val.app (op V)) := by
+  rw [toRestrict_app_val_all]
+  apply Presheaf.IsFlasque.map_epi
+
+lemma restrict_comp_pushforward_isFlasque [IsFlasque F] :
+    IsFlasque ((restrict C U.isOpenEmbedding ⋙ pushforward C U.inclusion').obj F) where
+  epi {U V i} := by
+    dsimp
+    apply Presheaf.IsFlasque.map_epi
+
+end TopCat.Sheaf
