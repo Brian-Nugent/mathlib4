6279acf9295167661e184fceed809f89c6878ca6
Brian-Nugent <bnugent@uw.edu>
2026-03-18 16:14:46 -0600

    Add longSequenceFunctor

--- Mathlib/CategoryTheory/Sites/SheafCohomology/ExactSequences.lean
+++ Mathlib/CategoryTheory/Sites/SheafCohomology/ExactSequences.lean
@@ -20,9 +20,14 @@ universe w' w v u
 
 namespace CategoryTheory
 
-open Abelian
+open Abelian AddCommGrpCat
 
 variable {C : Type u} [Category.{v} C] {J : GrothendieckTopology C}
+
+variable (C) in
+/-- The category of short exact sequences -/
+abbrev ShortExactSequences [Limits.HasZeroMorphisms C] :=
+    ObjectProperty.FullSubcategory (ShortComplex.ShortExact (C := C))
 
 namespace Sheaf
 
@@ -40,43 +45,90 @@ noncomputable def connectingHom (h : n₀ + 1 = n₁ := by omega) : H S.X₃ n�
 variable {S₁ S₂ : ShortComplex (Sheaf J AddCommGrpCat.{w})}
     (h₁ : S₁.ShortExact) (h₂ : S₂.ShortExact) (f : S₁ ⟶ S₂)
 
+set_option backward.isDefEq.respectTransparency false in
+@[simp]
 theorem connectingHom_naturality (h : n₀ + 1 = n₁ := by omega) (x : H S₁.X₃ n₀) :
-    connectingHom h₂ n₀ n₁ h (map f.τ₃ n₀ x) = map f.τ₁ n₁ (connectingHom h₁ n₀ n₁ h x) := sorry
-
-open AddCommGrpCat
+    connectingHom h₂ n₀ n₁ h (map f.τ₃ n₀ x) = map f.τ₁ n₁ (connectingHom h₁ n₀ n₁ h x) := by
+  delta connectingHom H map
+  simp [ShortComplex.ShortExact.extClass_naturality h₁ h₂ f]
 
 /-- The long exact sequence on sheaf cohomology. -/
-@[simps!]
 noncomputable def longSequence (h : n₀ + 1 = n₁ := by omega) :
     ComposableArrows AddCommGrpCat.{w'} 5 := ComposableArrows.mk₅
   (ofHom (H.map S.f n₀))
   (ofHom (H.map S.g n₀))
-  (ofHom (H.connectingHom hS n₀ n₁))
+  (ofHom (H.connectingHom hS n₀ n₁ h))
   (ofHom (H.map S.f n₁))
   (ofHom (H.map S.g n₁))
 
-attribute [local simp] map_comp_apply in
-noncomputable def longSequence_hom (h : n₀ + 1 = n₁ := by omega) :
-    longSequence h₁ n₀ n₁ h ⟶ longSequence h₂ n₀ n₁ h := by
-  fapply ComposableArrows.homMk₅
-  · exact (ofHom (map f.τ₁ n₀))
-  · exact (ofHom (map f.τ₂ n₀))
-  · exact (ofHom (map f.τ₃ n₀))
-  · exact (ofHom (map f.τ₁ n₁))
-  · exact (ofHom (map f.τ₂ n₁))
-  · exact (ofHom (map f.τ₃ n₁))
-  · simp
-    have := f.4
-
-
-    sorry
-  sorry
-  sorry
-  sorry
-  sorry
-
 theorem longSequence_exact (h : n₀ + 1 = n₁ := by omega) : (longSequence hS n₀ n₁ h).Exact :=
   Ext.covariantSequence_exact _ hS n₀ n₁ h
+
+/-- The induced homomorphism of long exact equences -/
+noncomputable def longSequence_hom (h : n₀ + 1 = n₁ := by omega) :
+    longSequence h₁ n₀ n₁ h ⟶ longSequence h₂ n₀ n₁ h := ComposableArrows.homMk₅
+  (ofHom (map f.τ₁ n₀))
+  (ofHom (map f.τ₂ n₀))
+  (ofHom (map f.τ₃ n₀))
+  (ofHom (map f.τ₁ n₁))
+  (ofHom (map f.τ₂ n₁))
+  (ofHom (map f.τ₃ n₁))
+  (by
+    have := congr_arg (functorH J n₀).map f.4
+    repeat rw [Functor.map_comp] at this
+    exact this.symm)
+  (by
+    have := congr_arg (functorH J n₀).map f.5
+    repeat rw [Functor.map_comp] at this
+    exact this.symm)
+  (by
+    ext x
+    simpa using (connectingHom_naturality n₀ n₁ h₁ h₂ f h x).symm)
+  (by
+    have := congr_arg (functorH J n₁).map f.4
+    repeat rw [Functor.map_comp] at this
+    exact this.symm)
+  (by
+    have := congr_arg (functorH J n₁).map f.5
+    repeat rw [Functor.map_comp] at this
+    exact this.symm)
+
+@[simp]
+lemma longSequence_hom_app_zero (h : n₀ + 1 = n₁ := by omega) :
+  (longSequence_hom n₀ n₁ h₁ h₂ f).app 0 = ofHom (map f.τ₁ n₀) := rfl
+
+@[simp]
+lemma longSequence_hom_app_one (h : n₀ + 1 = n₁ := by omega) :
+  (longSequence_hom n₀ n₁ h₁ h₂ f).app 1 = ofHom (map f.τ₂ n₀) := rfl
+
+@[simp]
+lemma longSequence_hom_app_two (h : n₀ + 1 = n₁ := by omega) :
+  (longSequence_hom n₀ n₁ h₁ h₂ f).app 2 = ofHom (map f.τ₃ n₀) := rfl
+
+@[simp]
+lemma longSequence_hom_app_three (h : n₀ + 1 = n₁ := by omega) :
+  (longSequence_hom n₀ n₁ h₁ h₂ f).app 3 = ofHom (map f.τ₁ n₁) := rfl
+
+@[simp]
+lemma longSequence_hom_app_four (h : n₀ + 1 = n₁ := by omega) :
+  (longSequence_hom n₀ n₁ h₁ h₂ f).app 4 = ofHom (map f.τ₂ n₁) := rfl
+
+@[simp]
+lemma longSequence_hom_app_five (h : n₀ + 1 = n₁ := by omega) :
+  (longSequence_hom n₀ n₁ h₁ h₂ f).app 5 = ofHom (map f.τ₃ n₁) := rfl
+
+/-- The long exact sequence of cohomology is functorial -/
+@[simps]
+noncomputable def longSequenceFunctor (h : n₀ + 1 = n₁ := by omega) :
+    ShortExactSequences (Sheaf J AddCommGrpCat.{w}) ⥤ ComposableArrows AddCommGrpCat.{w'} 5 where
+      obj S := longSequence S.property n₀ n₁ h
+      map {S₁ S₂} f := longSequence_hom n₀ n₁ S₁.property S₂.property f.hom h
+      map_id S := by
+        ext x
+        any_goals convert map_id_apply x
+      map_comp _ _ := by
+        ext x
+        any_goals convert map_comp_apply _ _ x
 
 lemma longSequence_exact₁' (h : n₀ + 1 = n₁ := by omega) :
     (ShortComplex.mk (ofHom (H.connectingHom hS n₀ n₁ h)) (ofHom (H.map S.f n₁)) (by
